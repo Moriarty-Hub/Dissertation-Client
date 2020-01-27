6b013524455f672f6c7048a771f2944309c99602
Moriarty-Hub <samdongxiangrui@outlook.com>
2020-01-27 21:53:16 +0800

    Create unit test cases for Monitor.py

--- Constants.py
+++ Constants.py
@@ -1,1 +1,11 @@
-ENV_WORKSPACE = ""
+ENV_WORKSPACE = None
+DATABASE_CONNECTION = None
+DATABASE_URL = "47.98.53.171"
+DATABASE_USERNAME = "dxr"
+DATABASE_PASSWORD = "dxr"
+DATABASE_NAME = "network_asset"
+DATABASE_CURSOR = None
+RAW_POC_LIST = "https://raw.githubusercontent.com/boy-hack/airbug/master/API.json"
+POC_INFO_TABLE_NAME = "poc_info"
+POC_INFO_TABLE_FIELDS = ["id", "name", "type", "file_path", "create_time", "url"]
+
--- Monitor.py
+++ Monitor.py
@@ -8,18 +8,76 @@ Task list:
 """
 
 import os
+import json
+import pymysql
+import requests
 
 import Constants
 
 
 # The monitor is consist of two threads, one for listening server's command, the other one for executing
 
-def exportEnvironmentVariable():
+def initializeConstantVariables():
     Constants.ENV_WORKSPACE = os.getcwd()
+    Constants.DATABASE_CONNECTION = pymysql.connect(Constants.DATABASE_URL, Constants.DATABASE_USERNAME,
+                                                    Constants.DATABASE_PASSWORD, Constants.DATABASE_NAME)
+    Constants.DATABASE_CURSOR = Constants.DATABASE_CONNECTION.cursor()
 
 
 def updatePocInfo():
+    updateDatabase()
+    updateLocalPocFile()
+
+
+def updateDatabase():
+    clearPocInfo()
+    pocList = collectPocInfoFromWebsite()
+    insertPocInfoIntoDatabase(pocList)
+
+
+def clearPocInfo():
+    deleteStatement = "DELETE FROM " + Constants.POC_INFO_TABLE_NAME
+    Constants.DATABASE_CONNECTION.execute(deleteStatement)
+    Constants.DATABASE_CONNECTION.commit()
+
+
+def collectPocInfoFromWebsite():
+    serverResponse = requests.get(Constants.RAW_POC_LIST)
+    pocList = json.loads(serverResponse.text)
+    return pocList
+
+
+def insertPocInfoIntoDatabase(pocInfo):
+    insertStatementTemplate = "INSERT INTO %s (%s, %s, %s, %s, %s)"
+    for pocItem in pocInfo:
+        insertStatement = insertStatementTemplate % (
+            Constants.POC_INFO_TABLE_NAME, pocItem[Constants.POC_INFO_TABLE_FIELDS[1]],
+            pocItem[Constants.POC_INFO_TABLE_FIELDS[2]], pocItem[Constants.POC_INFO_TABLE_FIELDS[3]],
+            pocItem[Constants.POC_INFO_TABLE_FIELDS[4]], pocItem[Constants.POC_INFO_TABLE_FIELDS[5]])
+        Constants.DATABASE_CURSOR.execute(insertStatement)
+        # The performance may be better if this statement was put out of the for-loop, but not sure if there is any
+        # error occurs.
+        Constants.DATABASE_CONNECTION.commit()
+
+
+def updateLocalPocFile():
+    removeLocalPocFile()
+    pocFileUrlList = collectPocFileUrlFromWebsite()
+    downloadPocFile(pocFileUrlList)
+
+
+def removeLocalPocFile():
     pass
 
 
-exportEnvironmentVariable()
+def collectPocFileUrlFromWebsite():
+    return []
+    pass
+
+
+def downloadPocFile(pocFileUrlList):
+    pass
+
+
+def releaseResources():
+    Constants.DATABASE_CONNECTION.close()
