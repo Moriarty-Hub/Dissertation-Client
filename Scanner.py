49e7047f7876144a313a17fb5922d1bb50a94762
Xiangrui Dong <samdongxiangrui@outlook.com>
2020-02-21 21:53:16 +0800

    Deleted the method of deleteInactiveUrlFromHostList.

--- Scanner.py
+++ Scanner.py
@@ -48,7 +48,6 @@ class Scanner(object):
         return pocInfoList
 
     def execute(self):
-        self.__deleteInactiveUrlFromUrlList()
         self.__deleteInactiveHostFromHostList()
         self.__clearDatabaseContent()
         self.__scanTargetsOfSpecifiedType("url")
@@ -56,22 +55,15 @@ class Scanner(object):
         self.__commitModificationToDatabase()
         self.__releaseResources()
 
-    def __deleteInactiveUrlFromUrlList(self):
-        self.__deleteInactiveTargetFromTargetList(self.__urlTargetsList)
-
     def __deleteInactiveHostFromHostList(self):
-        self.__deleteInactiveTargetFromTargetList(self.__hostTargetsList)
-
-    @staticmethod
-    def __deleteInactiveTargetFromTargetList(targetList):
-        targetListCopy = list(targetList)
+        targetListCopy = list(self.__hostTargetsList)
         for target in targetListCopy:
             os.system("ping -w 4 %s | tail -n 2 | head -n 1 > pingResult.txt" % target)
             file = open("pingResult.txt")
             result = file.read()
             file.close()
             if "0 received, 100% packet loss" in result:
-                targetList.remove(target)
+                self.__hostTargetsList.remove(target)
 
     def __clearDatabaseContent(self):
         deleteStatement = "DELETE FROM " + Constants.SCAN_RESULT_TABLE_NAME
